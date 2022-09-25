6ddfe67b77c1c8c7b8dc24530959c9a70de7fcff
gaoge <gaoge@kuaishou.com>
2022-09-25 21:21:43 +0800

    测试新合约-更新2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import MintPage from '../views/MintPage.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView,
+    component: MintPage,
   },
   {
     path: '/about',
@@ -15,14 +16,14 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
-  {
-    path: '/mint',
-    name: 'mint',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "mint" */ '../views/MintPage.vue'),
-  },
+  // {
+  //   path: '/mint',
+  //   name: 'mint',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "mint" */ '../views/MintPage.vue'),
+  // },
 ];
 
 const router = createRouter({
