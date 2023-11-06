51e1f878072f249f7433b7bccc84082d1d129091
gaoge <314013991@qq.com>
2023-11-06 15:42:19 +0800

    feat: mint重新上线

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,24 +6,24 @@ import MintPageWuJing from '../views/MintPageWuJing.vue';
 // import MintPageDev from '../views/MintPageDev.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: MintPageWuJing,
-  },
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: MintPageWuJing,
+  // },
   // {
   //   path: '/dev',
   //   name: 'dev',
   //   component: MintPageDev,
   // },
-  // {
-  //   path: '/mint',
-  //   name: 'mint',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "mint" */ '../views/MintPage.vue'),
-  // },
+  {
+    path: '/',
+    name: 'mint',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "mint" */ '../views/MintPage.vue'),
+  },
 ];
 
 const router = createRouter({
